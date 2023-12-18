4e3e932154afebe5c4d2e431c630abca7c1d548d
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2023-12-18 20:32:12 -0300

    Instalação do node e do npm

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const express = require("express");
 const cors = require("cors");
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose')
 const app = express();
 app.use(express.json());
 app.use(cors());
